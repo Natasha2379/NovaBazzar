81a54b68c670a5d3949c50e8bd3d9a5200ca340e
ijpkaushik <jaiprakash02082001@gmail.com>
2023-04-24 22:40:15 +0530

    extension added

--- novabazzar-frontend/src/pages/Home/Home.jsx
+++ novabazzar-frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./home.module.scss";
 
-import Topbar from "../../components/Topbar/Topbar";
-import FeaturedItems from "../../components/FeaturedItems/FeaturedItems";
-import FeaturedShops from "../../components/FeaturedShops/FeaturedShops";
-import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import Topbar from "../../components/Topbar/Topbar.jsx";
+import FeaturedItems from "../../components/FeaturedItems/FeaturedItems.jsx";
+import FeaturedShops from "../../components/FeaturedShops/FeaturedShops.jsx";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts.jsx";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
