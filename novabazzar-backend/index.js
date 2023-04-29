87c38a4b655ece773f88e60f70d0bc4513419168
ijpkaushik <jaiprakash02082001@gmail.com>
2023-04-29 13:55:09 +0530

    env added

--- novabazzar-backend/index.js
+++ novabazzar-backend/index.js
@@ -8,14 +8,14 @@ const cors = require("cors");
 const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
 const shopRouter = require("./routes/shop");
-const { PORT } = require("./config/dev");
-const { ATLAS_URI } = require("./config/dev");
+// const { PORT } = require("./config/dev");
+// const { ATLAS_URI } = require("./config/dev");
 
 const app = express();
 dotenv.config();
 
-const RUN_PORT = PORT || 5000;
-const URI = ATLAS_URI;
+const RUN_PORT = process.env.PORT || 5000;
+const URI = process.env.ATLAS_URI;
 
 mongoose.set("strictQuery", false);
 mongoose
